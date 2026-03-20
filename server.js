f19374316e06c4a7d739a6a96af44aef58df8b38
L11nl <alajnbyq@gmail.com>
2026-03-20 05:13:03 +0300

    Add CORS support and update server response messages

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+// الصفحة الرئيسية
+app.get('/', (req, res) => {
+res.send('API شغال 🔥');
+});
+
+// استبدال بطاقة
 app.post('/redeem', async (req, res) => {
 try {
 const { card_key } = req.body;
 
 const params = new URLSearchParams();
 params.append('card_key', card_key);
+params.append('merchant_dict_id', 1);
+params.append('platform_id', 1);
 
 const response = await axios.post(
   'https://api.node-card.com/api/open/card/redeem',
@@ -26,9 +37,11 @@ res.json(response.data);
 } catch (err) {
 res.json({
 code: 0,
-msg: "خطأ بالسيرفر"
+msg: "خطأ بالسيرفر ❌"
 });
 }
 });
 
-app.listen(3000, () => console.log('Server running'));
+app.listen(process.env.PORT || 3000, () => {
+console.log('Server running');
+});
