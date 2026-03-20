57dde1f3f3757fe036bf48cbb6fe603896b008ef
L11nl <alajnbyq@gmail.com>
2026-03-20 05:07:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
+
 app.use(express.json());
 
-const BASE = 'https://api.node-card.com';
-
 app.post('/redeem', async (req, res) => {
-  try {
-    const r = await axios.post(
-      BASE + '/api/open/card/redeem',
-      new URLSearchParams(req.body),
-      { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    );
-    res.json(r.data);
-  } catch {
-    res.json({ code: 0, msg: 'error' });
+try {
+const { card_key } = req.body;
+
+const params = new URLSearchParams();
+params.append('card_key', card_key);
+
+const response = await axios.post(
+  'https://api.node-card.com/api/open/card/redeem',
+  params,
+  {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
   }
+);
+
+res.json(response.data);
+
+} catch (err) {
+res.json({
+code: 0,
+msg: "خطأ بالسيرفر"
+});
+}
 });
 
-app.get('/', (req, res) => {
-  res.send('API شغال 🔥');
-});
-
-app.listen(3000);
+app.listen(3000, () => console.log('Server running'));
