092fd6f01a025e15e11c6ba0e7b9b820e40e2cd1
L11nl <alajnbyq@gmail.com>
2026-03-25 13:43:50 +0300

    Create merchants.js

---
+++ data/merchants.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { id: "4", en: "Spotify", ar: "سبوتيفاي" },
+  { id: "5", en: "YouTube", ar: "يوتيوب" },
+  { id: "6", en: "ChatGPT", ar: "شات جي بي تي" },
+  { id: "7", en: "Amazon", ar: "أمازون" }
+];
