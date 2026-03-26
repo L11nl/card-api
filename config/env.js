7ec4cd01251744cda384a7d83de50af3304bd7de
L11nl <alajnbyq@gmail.com>
2026-03-26 07:04:07 +0300

    Delete config directory

--- config/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  TOKEN: process.env.BOT_TOKEN || process.env.TOKEN,
-  WALLET: process.env.WALLET || "PUT_TRC20_ADDRESS",
-  ADMIN_ID: Number(process.env.ADMIN_ID || 643309456),
-  PORT: Number(process.env.PORT || 3000),
-  PRICE: Number(process.env.PRICE || 2.5)
-};
